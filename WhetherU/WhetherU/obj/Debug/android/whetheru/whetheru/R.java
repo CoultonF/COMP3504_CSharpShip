50505e1194cb4d4c0247b57f19471d7edd752dca
CoultonF <cjrfraser@gmail.com>
2016-10-17 15:27:16 -0600

    Added OAuth reference

--- WhetherU/WhetherU/obj/Debug/android/whetheru/whetheru/R.java
+++ WhetherU/WhetherU/obj/Debug/android/whetheru/whetheru/R.java
@@ -20,7 +20,8 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int ApplicationName=0x7f040001;
-        public static final int Hello=0x7f040000;
+        public static final int ApplicationName=0x7f040002;
+        public static final int Hello=0x7f040001;
+        public static final int library_name=0x7f040000;
     }
 }
