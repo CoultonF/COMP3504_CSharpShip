9eb6233c276d3bda027cce3b8312d52d775168a4
Priyash Bista <priyashbista@gmail.com>
2016-10-31 17:01:35 -0600

    Add files via upload

---
+++ WeatherAPI/WeatherInfo/obj/WeatherTest/android/src/md5c9d0e8d5bf32a5f6ce25826781e56bbc/ForecastAdapterClass_ViewHolderClass.java
@@ -0,0 +1,38 @@
+package md5c9d0e8d5bf32a5f6ce25826781e56bbc;
+
+
+public class ForecastAdapterClass_ViewHolderClass
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("WeatherInfo.ForecastAdapterClass+ViewHolderClass, WeatherInfo, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", ForecastAdapterClass_ViewHolderClass.class, __md_methods);
+	}
+
+
+	public ForecastAdapterClass_ViewHolderClass () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == ForecastAdapterClass_ViewHolderClass.class)
+			mono.android.TypeManager.Activate ("WeatherInfo.ForecastAdapterClass+ViewHolderClass, WeatherInfo, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
