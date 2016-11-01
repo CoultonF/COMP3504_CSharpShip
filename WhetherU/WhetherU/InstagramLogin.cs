a0d3adfae6b0ac5184f4fdc370dd56f4b6b9fcbe
CoultonF <cjrfraser@gmail.com>
2016-10-31 19:56:20 -0600

    Tried to get it working but didnt

--- WhetherU/WhetherU/InstagramLogin.cs
+++ WhetherU/WhetherU/InstagramLogin.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using InstaSharp;
-using Auth0;
-using Xamarin.Auth;
 
 using Android.App;
 using Android.Content;
@@ -15,29 +13,28 @@ using Android.Widget;
 
 namespace WhetherU
 {
-    [Activity(Label = "WhetherU", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "Activity1")]
+    public class InstagramLogin : Activity
     {
-
-        protected override async void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            // Create your application here
             var clientId = "33665d08e62942c6b1f484f422bbb7c1";
             var clientSecret = "ae94378b39e747b0ac5d6f8640636b02 ";
-            var redirectUri = "";
+            var redirectUri = "https://elfsight.com/service/generate-instagram-access-token/";
             var realtimeUri = "";
 
             InstagramConfig config = new InstagramConfig(clientId, clientSecret, redirectUri, realtimeUri);
-
 
             var scopes = new List<OAuth.Scope>();
             scopes.Add(InstaSharp.OAuth.Scope.Likes);
             scopes.Add(InstaSharp.OAuth.Scope.Comments);
 
             var link = InstaSharp.OAuth.AuthLink(config.OAuthUri + "authorize", config.ClientId, config.RedirectUri, scopes, InstaSharp.OAuth.ResponseType.Code);
+            
         }
+
     }
 }
-
