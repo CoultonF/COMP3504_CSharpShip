786bdb44c52b915f81392f6b28e5cb85cc4c1364
CoultonF <cjrfraser@gmail.com>
2016-11-02 16:09:47 -0600

    Got the instagram login working

--- WhetherU/WhetherU/MainActivity.cs
+++ WhetherU/WhetherU/MainActivity.cs
@@ -18,6 +18,7 @@ namespace WhetherU
     [Activity(Label = "WhetherU", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        public Intent MainLaunch { get; private set; }
 
         protected override async void OnCreate(Bundle bundle)
         {
@@ -26,17 +27,47 @@ namespace WhetherU
             // Set our view from the "main" layout resource
             var clientId = "33665d08e62942c6b1f484f422bbb7c1";
             var clientSecret = "ae94378b39e747b0ac5d6f8640636b02 ";
-            var redirectUri = "";
+            var redirectUri = "https://elfsight.com/service/generate-instagram-access-token/";
             var realtimeUri = "";
 
-            InstagramConfig config = new InstagramConfig(clientId, clientSecret, redirectUri, realtimeUri);
+            var auth = new OAuth2Authenticator(
+                clientId: "33665d08e62942c6b1f484f422bbb7c1",
+                scope: "basic",
+                authorizeUrl: new Uri("https://api.instagram.com/oauth/authorize/"),
+                redirectUrl: new Uri("https://elfsight.com/service/generate-instagram-access-token/"));
 
+            StartActivity(auth.GetUI(this));
+            //PresentViewController(auth.GetUI(), true, null);
+            IEnumerable<Account> accounts = AccountStore.Create(this).FindAccountsForService("Instagram");
+            auth.Completed += (sender, eventArgs) => {
+                    // We presented the UI, so it's up to us to dimiss it on iOS.
+                    //DismissViewController(true, null);
 
-            var scopes = new List<OAuth.Scope>();
-            scopes.Add(InstaSharp.OAuth.Scope.Likes);
-            scopes.Add(InstaSharp.OAuth.Scope.Comments);
+                    if (eventArgs.IsAuthenticated)
+                    {
+                    // Use eventArgs.Account to do wonderful things
+                    AccountStore.Create(this).Save(eventArgs.Account, "Instagram");
 
-            var link = InstaSharp.OAuth.AuthLink(config.OAuthUri + "authorize", config.ClientId, config.RedirectUri, scopes, InstaSharp.OAuth.ResponseType.Code);
+                    var request = new OAuth2Request("GET", new Uri("https://api.instagram.com/v1/tags/{tag-name}?access_token=ACCESS-TOKEN"), null, eventArgs.Account);
+                    request.GetResponseAsync().ContinueWith(t => {
+                        if (t.IsFaulted)
+                            Console.WriteLine("Error: " + t.Exception.InnerException.Message);
+                        else
+                        {
+                            string json = t.Result.GetResponseText();
+                            Console.WriteLine(json);
+                        }
+                    });
+                    Console.WriteLine(request);
+                    SetContentView(Resource.Layout.Main);
+                }
+                    else
+                    {
+                        // The user cancelled
+                    }
+                };
+
+            
         }
     }
 }
